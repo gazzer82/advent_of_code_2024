57302c3cfe2624b8b3d078543d69a6c125d34553
gazzer82 <contact@garethjeanne.co.uk>
2024-12-07 07:42:37 +0000

    Day 06 Optimisation

--- lib/aoc_2024/solutions/y24/day06.ex
+++ lib/aoc_2024/solutions/y24/day06.ex
@@ -87,45 +87,70 @@ defmodule Aoc2024.Solutions.Y24.Day06 do
     matrix = problem |> build_matrix()
     start = find_start_position(matrix)
 
+    :persistent_term.put(Matrix, matrix)
+
     options =
       find_start_position(matrix)
       |> move(matrix, :up)
       |> Map.to_list()
       |> Enum.filter(fn {_, v} -> v == "X" end)
 
-    Enum.map(options, fn {{x, y}, _} ->
-      Task.async(fn ->
-        test_matrix = Map.put(matrix, {x, y}, "#")
-        move_2(start, test_matrix, :up, MapSet.new())
+    options
+    |> split_into_chunks()
+    |> Task.async_stream(fn options ->
+      Enum.map(options, fn {{x, y}, _} ->
+        # test_matrix = Map.put(matrix, {x, y}, "#")
+        move_2(start, :up, MapSet.new(), {x, y})
       end)
     end)
-    |> Task.await_many(:infinity)
+    |> merge_results_stream()
     |> Enum.filter(& &1)
     |> Enum.count()
   end
 
-  def move_2(current_position, matrix, direction, previous) do
+  defp split_into_chunks(options) do
+    workers = :erlang.system_info(:schedulers_online)
+    options_count = Enum.count(options)
+    options_per_chunk = :erlang.ceil(options_count / workers)
+
+    Enum.chunk_every(options, options_per_chunk)
+  end
+
+  defp merge_results_stream(results_stream) do
+    Enum.reduce(results_stream, [], fn {:ok, worker_result}, acc ->
+      acc ++ worker_result
+    end)
+  end
+
+  def move_2(current_position, direction, previous, testing_position) do
     if check_been_here_before(current_position, direction, previous) do
       true
     else
       next_coord = next_coord(current_position, direction)
+      matrix = :persistent_term.get(Matrix)
 
       case Map.get(matrix, next_coord) do
         nil ->
           false
 
         square ->
-          {next_position, matrix, direction, backtrack} =
-            check_next_position_2(square, direction, matrix, next_coord, current_position)
+          {next_position, direction, backtrack} =
+            check_next_position_2(
+              square,
+              direction,
+              next_coord,
+              current_position,
+              testing_position
+            )
 
           if backtrack do
-            move_2(current_position, matrix, direction, previous)
+            move_2(current_position, direction, previous, testing_position)
           else
             move_2(
               next_position,
-              matrix,
               direction,
-              MapSet.put(previous, {current_position, direction})
+              MapSet.put(previous, {current_position, direction}),
+              testing_position
             )
           end
       end
@@ -136,12 +161,35 @@ defmodule Aoc2024.Solutions.Y24.Day06 do
     MapSet.member?(previous, {current_position, direction})
   end
 
-  def check_next_position_2(square, direction, matrix, next_position, _current_position)
-      when square in [".", "X", "^"] do
-    {next_position, Map.put(matrix, next_position, "X"), direction, false}
+  def check_next_position_2(
+        _square,
+        direction,
+        next_x_y,
+        current_position,
+        testing_position
+      )
+      when next_x_y == testing_position do
+    {current_position, rotate_direction(direction), true}
   end
 
-  def check_next_position_2(_square, direction, matrix, _next_position, current_position) do
-    {current_position, matrix, rotate_direction(direction), true}
+  def check_next_position_2(
+        square,
+        direction,
+        next_position,
+        _current_position,
+        _testing_position
+      )
+      when square in [".", "X", "^"] do
+    {next_position, direction, false}
+  end
+
+  def check_next_position_2(
+        _square,
+        direction,
+        _next_position,
+        current_position,
+        _testing_position
+      ) do
+    {current_position, rotate_direction(direction), true}
   end
 end
