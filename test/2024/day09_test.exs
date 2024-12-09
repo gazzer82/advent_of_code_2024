b8dd5904aada485eff19d4f5a9689fb43dec9bc1
gazzer82 <contact@garethjeanne.co.uk>
2024-12-09 20:52:53 +0000

    Enable tests

--- test/2024/day09_test.exs
+++ test/2024/day09_test.exs
@@ -12,19 +12,19 @@ defmodule Aoc2024.Solutions.Y24.Day09Test do
     apply(Solution, part, [problem])
   end
 
-  # test "part one example" do
-  #   input = ~S"""
-  #   2333133121414131402
-  #   """
+  test "part one example" do
+    input = ~S"""
+    2333133121414131402
+    """
 
-  #   assert 1928 == solve(input, :part_one)
-  # end
+    assert 1928 == solve(input, :part_one)
+  end
 
-  # @part_one_solution 6_200_294_120_911
+  @part_one_solution 6_200_294_120_911
 
-  # test "part one solution" do
-  #   assert {:ok, @part_one_solution} == AoC.run(2024, 9, :part_one)
-  # end
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2024, 9, :part_one)
+  end
 
   test "part two example" do
     input = ~S"""
